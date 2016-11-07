3a7c8b99502de89c0479755e46a40206e9e0b11b
Greg Brockman <gdb@gregbrockman.com>
2016-11-07 11:55:59 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ class Build(DistutilsBuild):
 
 setup(
     name='fastzbarlight',
-    version='0.0.8',
+    version='0.0.9',
     description="A fork of zbarlight, which includes a vendored copy of zbar compiled with optimization flags",
     long_description=read('README.rst'),
     classifiers=[
